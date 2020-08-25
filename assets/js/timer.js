d7c03573cd00d4b4abff41bb735a8ba36e32a586
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-08-25 21:59:30 +0530

    Improved timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -3,15 +3,28 @@ var input = {
     minutes: 0,
     seconds: 0
 };
+var stopwatch
 
-var timestamp = new Date(input.hours, input.minutes, input.seconds);
+function timer() {
+    var timestamp = new Date(input.hours, input.minutes, input.seconds);
+    
+    var interval = 1;
+    
+    stopwatch = setInterval(function () {
+        timestamp = new Date(timestamp.getTime() + interval * 1000);
+        input.minutes = timestamp.getMinutes()
+        input.hours = timestamp.getHours()
+        input.seconds = timestamp.getSeconds()
+        document.getElementById('countdown2').innerHTML = input.hours + 'h:' + input.minutes + 'm:' + input.seconds + 's';
+    }, Math.abs(interval) * 1000);
 
-var interval = 1;
+}
 
-setInterval(function () {
-    timestamp = new Date(timestamp.getTime() + interval * 1000);
-    input.minutes = timestamp.getMinutes()
-    input.hours = timestamp.getHours()
-    input.seconds = timestamp.getSeconds()
-    document.getElementById('countdown2').innerHTML = timestamp.getHours() + 'h:' + timestamp.getMinutes() + 'm:' + timestamp.getSeconds() + 's';
-}, Math.abs(interval) * 1000);
+function resetTimer(){
+    input.seconds = 0
+    input.minutes = 0
+    input.hours = 0
+    clearInterval(stopwatch)
+    document.getElementById('countdown2').innerHTML = '0h:0m:0s';
+}
+
