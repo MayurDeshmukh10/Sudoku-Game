8d52a7c06410374d0b02681a560df67ce312146d
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-08-25 21:58:58 +0530

    Added js for leaderboard, difficulty selection

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,40 +1,74 @@
 var conn
-function loadGame() {
+function changeDifficultyLevel(){
+    resetTimer()
+    var level = document.getElementById("level").value
+    var table = document.getElementById("grid")
+    var ol = document.getElementById("scoreBoard")
+    if(table != null) {
+        while(table.firstChild) {
+            table.removeChild(table.firstChild);
+        }
+    }
+    while(ol.firstChild) {
+        ol.removeChild(ol.firstChild);
+    }
+    console.log("Level selected : ",level)
+    loadGame(level)
+}
+async function loadGame(level) {
     conn = new WebSocket("ws://" + document.location.host + "/ws");
     conn.close = function () {
-    console.log("Connection with Server closed")
+        console.log("Connection with Server closed")
     }
+    await new Promise(r => setTimeout(r, 500));
+    conn.send(level)
     conn.onmessage = function (server_data) {
+        var ol = document.getElementById("scoreBoard")
         var message = server_data.data
-        console.log("Message len :", message.length)
-        var tr;
-        var tbl = document.getElementById("grid")
-        for (var i = 0; i < message.length; i++) {
-            if (i % 9 == 0) {
-                console.log("In tr")
-                tr = document.createElement("tr")
-                tbl.appendChild(tr)
+        
+        console.log("Message : ",message)
+        if( message.length > 81) {
+            var obj = JSON.parse(message)
+            for (var i = 0; i < obj.length; i++) {
+                var score = obj[i];
+                var li = document.createElement("li")
+                li.innerHTML = score.Name + " - " + score.Time
+                ol.appendChild(li)
             }
-            var td = document.createElement('td')
-            // td.innerHTML = message[i]
-            if (message[i] == '0') {
-                td.innerHTML = "<input id='cell-" + i + "'  type='number' min='1' max='9'  value='' onchange='sendMessage(" + i + ")'>"
-            } else {
-                td.innerHTML = "<input id='cell-" + i + "'  type='text' value='" + message[i] + "'disabled>"
+        } else {
+            if(message == null) {
+                console.log("in null")
+                var li = document.createElement("li")
+                li.innerHTML = "No Data Present"
+                ol.appendChild(li)
             }
+            var tr;
+            var tbl = document.getElementById("grid")
+            for (var i = 0; i < message.length; i++) {
+                if (i % 9 == 0) {
+                    tr = document.createElement("tr")
+                    tbl.appendChild(tr)
+                }
+                var td = document.createElement('td')
+                // td.innerHTML = message[i]
+                if (message[i] == '0') {
+                    td.innerHTML = "<input id='cell-" + i + "'  type='number' min='1' max='9'  value='' onchange='sendMessage(" + i + ")'>"
+                } else {
+                    td.innerHTML = "<input id='cell-" + i + "'  type='text' value='" + message[i] + "'disabled>"
+                }
 
-            tr.appendChild(td)
-            // console.log(message[i])
-
+                tr.appendChild(td)
+            }
+            timer("start")
         }
-        console.log("Server : ", message)
+        // console.log("Server : ", message)
     }
-
+    
 }
         
 window.onload = function () {
     if (window["WebSocket"]) {
-        loadGame()
+        loadGame(0)
     } else {
         console.log("Your Browser does not websocket")
         alert("Your Browser does not websocket")
@@ -43,13 +77,21 @@ window.onload = function () {
 
         
 function resetGame() {
-    location.reload(); 
+    resetTimer()
+    var table = document.getElementById("grid")
+    var ol = document.getElementById("scoreBoard")
+    if(table != null) {
+        while(table.firstChild) {
+            table.removeChild(table.firstChild);
+        }
+    }
+    while(ol.firstChild) {
+        ol.removeChild(ol.firstChild);
+    }
+    loadGame(0)
 }
-        
-function sendMessage(id) {
 
-    console.log("Time : ",input.minutes)
-            
+async function sendMessage(id) {
     var value = document.getElementById("cell-" + id).value
             
     document.getElementById("cell-"+id).style.background = 'white'
@@ -60,19 +102,39 @@ function sendMessage(id) {
         row = parseInt(id)/9
         row = Math.floor(row)
         col = parseInt(id)%9
-        conn.send(value + ',' + row + ',' + col)
+        var userData = {
+            "value": parseInt(value),
+            "row": parseInt(row),
+            "col": parseInt(col)
+        }
+        conn.send(JSON.stringify(userData))
         conn.onmessage = function (server_data) {
-        console.log(server_data.data)
         if(server_data.data == 'violation'){
             document.getElementById("cell-"+id).style.background = 'red'
         } else if(server_data.data == 'win') {
-            Swal.fire({
-            title: 'You Won !',
-            imageUrl: 'https://res.cloudinary.com/mayur-cloud/image/upload/c_scale,h_400,w_600/v1597923424/trophy_pyhh2f.jpg',
-            imageWidth: 400,
-            imageHeight: 300,
-            imageAlt: 'Winner',
-            })
+            (async() => {
+                const {value: name} = await Swal.fire({
+                    title: 'You Won !',
+                    imageUrl: 'https://res.cloudinary.com/mayur-cloud/image/upload/c_scale,h_400,w_600/v1597923424/trophy_pyhh2f.jpg',
+                    imageWidth: 400,
+                    imageHeight: 300,
+                    imageAlt: 'Winner',
+                    input: 'text',
+                    text: 'Enter your Name',
+                    inputAttributes: {
+                        autocapitalize: 'off'
+                    },
+                    showCancelButton: false,
+                    confirmButtonText: 'Submit',
+                    inputValidator: (value) => {
+                        if (!value) {
+                          return 'Enter your Name'
+                        }
+                    }
+                })
+                conn.send(name)
+                loadGame(0)
+              })();             
         }
                     
     }
